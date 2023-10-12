897fa90576d29868df8b7a6b7e7e2c6ffa87f71b
LabA1_PC11 <cadizsalanovajarodzarian@gmail.com>
2023-10-12 09:45:52 -0400

    lol

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@
 public class Main {
     public static void main (String args[]) {
         examen sum1 = new examen();
-        sum1.calcularSuma(333,80);
+        sum1.calcularSuma(2,5);
         examen sum2 = new examen();
-        sum2.calcularSuma(5.9,42.8);
-        examen sum3 = new examen();
-        sum3.calcularSuma(50.6,20,120);
+        sum2.calcularSuma(6,5,8);
+        examen sum3=new examen();
+        sum3.calcularSuma(5.5,3.6,89.5);
+        examen sum4=new examen();
+        sum4.calcularSuma(6.5,8.5,8.4);
     }
 }
--- src/examen.java
+++ src/examen.java
@@ -1,12 +1,29 @@
 public class examen {
-    int sument1,sument2;
-    double sumdou1,sumdou2,sumdou3,total;
+    int sument1,sument2,sument3;
+    double sumdou1,sumdou2,sumdou3,total,total1;
 
     public examen() {
 
     }
 
+    public int getSument3() {
+        return sument3;
+    }
+
+    public void setSument3(int sument3) {
+        this.sument3 = sument3;
+    }
+
+    public double getTotal1() {
+        return total1;
+    }
+
+    public void setTotal1(double total1) {
+        this.total1 = total1;
+    }
+
     public double getTotal() {
+
         return total;
     }
 
@@ -54,25 +71,34 @@ public class examen {
         this.sumdou3 = sumdou3;
     }
 
-    public examen(int sument1, int sument2, double sumdou1, double sumdou2, double sumdou3,double total) {
+    public examen(int sument1, int sument2, double sumdou1, double sumdou2, double sumdou3,double total,double total1) {
         this.sument1 = sument1;
         this.sument2 = sument2;
+        this.sument3=sument3;
         this.sumdou1 = sumdou1;
         this.sumdou2 = sumdou2;
         this.sumdou3 = sumdou3;
         this.total= total;
+        this.total1= total1;
     }
 
     public void calcularSuma(int sument1,int sument2){
         total=sument1+sument2;
-        System.out.println("La suma de los enteros es: "+ total);
+        System.out.println("La suma de los 2 valores enteros es: "+ total);
+    }
+    public void calcularSuma(int sument1,int sument2,int sument3){
+        total1=sument1+sument2+sument3;
+        System.out.println("la suma de los 3 valores enteros es: "+total1);
     }
     public void calcularSuma(double sumdou1,double sumdou2){
         total=sumdou1+sumdou2;
-        System.out.println("La suma de los dos valores es: "+total);
+
+
+        System.out.println("La suma de los 2 valores doubles es: "+total);
     }
-    public void calcularSuma(double sumdou1,double sumdou2, double sumdou3){
-        total=sumdou1+sumdou2+sumdou3;
-        System.out.println("La suma de los tres valores es: "+total);
+    public void calcularSuma(double sumdou1,double sumdou2,double sumdou3){
+        total1=sumdou1+sumdou2+sumdou3;
+        System.out.println("la suma de 3 valores double es: "+total1);
     }
+
 }
